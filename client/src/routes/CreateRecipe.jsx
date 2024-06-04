de311351c93bab5a265a22d825b30f2f17820505
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-06-04 13:45:57 -0400

    added route for creating recipe

---
+++ client/src/routes/CreateRecipe.jsx
@@ -0,0 +1,17 @@
+import NavBar from "../components/Navbar.jsx";
+import "../styles/Index.css"
+function CreateRecipe() {
+
+    return (
+        <>
+            <div className={"container"}>
+                <NavBar />
+                <div className="content">
+                    <p>This is the home page</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CreateRecipe;
