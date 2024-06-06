b34513ca7513f4601305418174b7ba21d345cb93
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-06-06 18:39:32 -0400

    all functionality working for search page

--- client/src/routes/MyRecipes.jsx
+++ client/src/routes/MyRecipes.jsx
@@ -3,11 +3,24 @@ import React, { useState, useEffect } from "react";
 import "../styles/MyRecipes.css";
 import axios from "axios";
 import {
-    Flex, Center, Select, CardBody, Card, Box, Text, Image,
+    Flex,
+    Center,
+    Select,
+    CardBody,
+    Card,
+    Box,
+    Text,
+    Image,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader, ModalBody, ModalFooter,
 } from "@chakra-ui/react";
 import { getAuth } from "firebase/auth";
 import "../styles/Index.css";
 import genericFoodImage from "../assets/genericFood.png";
+import {Link} from "react-router-dom";
 
 /*
     The goal of this page is to give users the options to display recipes that fall into the following categories:
@@ -24,6 +37,9 @@ import genericFoodImage from "../assets/genericFood.png";
 function MyRecipes() {
     const auth = getAuth();
     const user_id = auth.currentUser.uid;
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [selectedRecipe, setSelectedRecipe] = useState(null);
 
     const [favoriteRecipes, setFavoriteRecipes] = useState([]);
     const [createdRecipes, setCreatedRecipes] = useState([]);
@@ -47,7 +63,6 @@ function MyRecipes() {
                         params: { user_id },
                     }
                 );
-                console.log(responseCreated.data);
                 setCreatedRecipes(responseCreated.data);
                 setFavoriteRecipes(responseFavorites.data);
                 setFavoriteRecipesFromOtherUsers(getFavoritesFromOtherUsers(responseCreated.data, responseFavorites.data));
@@ -72,7 +87,6 @@ function MyRecipes() {
     const handleChange = (event) => {
         const value = event.target.value;
         setSelectedValue(value);
-        console.log(value);
         value === "userCreated"
             ? setCurrentlyDisplayedRecipes(createdRecipes)
             : value === "edamamCreated"
@@ -84,8 +98,10 @@ function MyRecipes() {
                         : setCurrentlyDisplayedRecipes(favoriteRecipes);
     };
 
-    function handleOpenModal(){
-        console.log("open")
+    function handleOpenModal(recipe){
+        console.log(recipe)
+        setSelectedRecipe(recipe);
+        onOpen();
     }
 
     return (
@@ -118,13 +134,13 @@ function MyRecipes() {
                                 Your created recipes
                             </option>
                             <option value="edamamCreated">
-                                Your saved Edamam recipes
+                                Your favorited Edamam recipes
                             </option>
                             <option value="otherUserCreated">
-                                Your saved recipes created by other users
+                                Your favorited recipes from other users
                             </option>
                             <option value="allSaved">
-                                All saved recipes
+                                All favorited recipes
                             </option>
                         </Select>
                     </Center>
@@ -147,7 +163,7 @@ function MyRecipes() {
                                 borderRadius="15"
                                 marginTop="30px"
                                 marginRight="1em"
-                                onClick={handleOpenModal}
+                                onClick={() => handleOpenModal(item)}
                             >
                                 <CardBody>
                                     <Image
@@ -172,6 +188,141 @@ function MyRecipes() {
                                     </Box>
                                 </CardBody>
                             </Card>
+                            {selectedRecipe && (
+                                <Modal
+                                    isOpen={isOpen}
+                                    onClose={onClose}
+                                    borderRadius="20px"
+                                >
+                                    <ModalOverlay
+                                        style={{
+                                            backgroundColor:
+                                                "rgba(0, 0, 0, 0.02)",
+                                        }}
+                                    />
+                                    <ModalContent
+                                        marginTop="20%"
+                                        marginLeft="36%"
+                                        width="400px"
+                                        height="300px"
+                                        bg="#EADDCF"
+                                        borderRadius="20px"
+                                    >
+                                        <ModalHeader
+                                            textAlign="center"
+                                            color="55423D"
+                                            marginTop=".3em"
+                                            fontSize="1.5em"
+                                        >
+                                            {selectedRecipe.name}
+                                        </ModalHeader>
+                                        <ModalBody marginLeft="1em">
+                                            <div
+                                                style={{
+                                                    position: "absolute",
+                                                    top: "5rem",
+                                                    left: ".5rem",
+                                                    paddingLeft: ".1rem",
+                                                    paddingRight: ".1rem",
+                                                }}
+                                            >
+                                                Ingredients:{"   "}
+                                                {selectedRecipe.userCreated
+                                                    ? selectedRecipe.ingredients.map(
+                                                        (ingredient) => (
+                                                            <div
+                                                                key={
+                                                                    ingredient.name
+                                                                }
+                                                            >
+                                                                <p>
+                                                                    {
+                                                                        ingredient.amount
+                                                                    }{" "}
+                                                                    {
+                                                                        ingredient.units
+                                                                    }{" "}
+                                                                    {
+                                                                        ingredient.name
+                                                                    }
+                                                                </p>
+                                                            </div>
+                                                        )
+                                                    )
+                                                    : selectedRecipe.ingredients.map(
+                                                        (
+                                                            ingredientLine,
+                                                            index
+                                                        ) => (
+                                                            <div
+                                                                key={
+                                                                    index
+                                                                }
+                                                            >
+                                                                <p>
+                                                                    {
+                                                                        ingredientLine.text
+                                                                    }
+                                                                </p>
+                                                            </div>
+                                                        )
+                                                    )}
+                                            </div>
+                                            <br />
+                                            <div
+                                                style={{
+                                                    position: "absolute",
+                                                    bottom: "2.5rem",
+                                                    left: ".5rem",
+                                                }}
+                                            >
+                                                Servings:{" "}
+                                                {selectedRecipe.servings}
+                                            </div>
+                                            <br />
+                                            <div
+                                                style={{
+                                                    position: "absolute",
+                                                    bottom: ".9rem",
+                                                    left: ".5rem",
+                                                }}
+                                            >
+                                                Calories/serving:{" "}
+                                                {(
+                                                    selectedRecipe.calories /
+                                                    selectedRecipe.servings
+                                                ).toFixed(2)}
+                                            </div>
+                                        </ModalBody>
+                                        <ModalFooter>
+                                            <Link to={selectedRecipe.userCreated ? `/recipe/${selectedRecipe.id}/firebase` : `/recipe/${selectedRecipe.id}/edamam`}>
+                                                <button
+                                                    style={{
+                                                        backgroundColor:
+                                                            "#55423D",
+                                                        color: "#EADDCF",
+                                                        borderRadius: "1em",
+                                                        height: "2em",
+                                                        width: "9em",
+                                                        border: "none",
+                                                        position:
+                                                            "absolute",
+                                                        bottom: ".6rem",
+                                                        right: ".5rem",
+                                                        display: "flex",
+                                                        justifyContent:
+                                                            "center",
+                                                        alignItems:
+                                                            "center",
+                                                    }}
+                                                >
+                                                    See full recipe
+                                                </button>
+                                            </Link>
+                                        </ModalFooter>
+                                    </ModalContent>
+                                </Modal>
+                            )}
                         </div>
                     ))}
                 </div>
